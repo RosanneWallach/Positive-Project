ec92dfded3981df4812b688a1a42f63ccb58e6d1
Rosie Vallakia <44302307+Chicaknowstech@users.noreply.github.com>
2021-01-12 15:26:17 -0500

    added app bar 3 componets done

--- streaming-service/src/App.js
+++ streaming-service/src/App.js
@@ -21,6 +21,18 @@ function App() {
     </div>
   );
 }
+<AppBar position="static">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      Welcome
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
+
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
