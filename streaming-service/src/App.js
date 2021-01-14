0807dbb515322b3cd24b40095d9e90cb6e2c74ad
BijornCoote <bijornc@gmail.com>
2021-01-13 23:46:46 -0500

    final layout

--- streaming-service/src/App.js
+++ streaming-service/src/App.js
@@ -8,7 +8,6 @@ import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Avatar from "@material-ui/core/Avatar";
 import Rating from "@material-ui/lab/Rating";
